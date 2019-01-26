1273642314638d81ed53a1f5d9e3148a936dda93
xicocana <36681727+xicocana@users.noreply.github.com>
2019-01-26 15:50:33 +0000

    Add files via upload

---
+++ RocketPlayer/RocketPlayer/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace RocketPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timerW = new System.Windows.Forms.Timer(this.components);
+            this.timerAD = new System.Windows.Forms.Timer(this.components);
+            this.timerLMB = new System.Windows.Forms.Timer(this.components);
+            this.timerRBM = new System.Windows.Forms.Timer(this.components);
+            this.timerEnter = new System.Windows.Forms.Timer(this.components);
+            this.timerR = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Drive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timerW
+            // 
+            this.timerW.Tick += new System.EventHandler(this.timerW_Tick);
+            // 
+            // timerAD
+            // 
+            this.timerAD.Interval = 5000;
+            this.timerAD.Tick += new System.EventHandler(this.timerAD_Tick);
+            // 
+            // timerLMB
+            // 
+            this.timerLMB.Interval = 9000;
+            this.timerLMB.Tick += new System.EventHandler(this.timerLMB_Tick);
+            // 
+            // timerRBM
+            // 
+            this.timerRBM.Interval = 7000;
+            this.timerRBM.Tick += new System.EventHandler(this.timerRBM_Tick);
+            // 
+            // timerEnter
+            // 
+            this.timerEnter.Interval = 1000;
+            this.timerEnter.Tick += new System.EventHandler(this.timerEnter_Tick);
+            // 
+            // timerR
+            // 
+            this.timerR.Interval = 1000;
+            this.timerR.Tick += new System.EventHandler(this.timerR_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(151, 135);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timerW;
+        private System.Windows.Forms.Timer timerAD;
+        private System.Windows.Forms.Timer timerLMB;
+        private System.Windows.Forms.Timer timerRBM;
+        private System.Windows.Forms.Timer timerEnter;
+        private System.Windows.Forms.Timer timerR;
+    }
+}
+
---
+++ RocketPlayer/RocketPlayer/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RocketPlayer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        static Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(500);
+            timerW.Enabled = true;
+            timerAD.Enabled = true;
+            timerLMB.Enabled = true;
+            timerRBM.Enabled = true;
+            timerEnter.Enabled = true;
+            timerR.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            keybd_event((byte)0x57, 0, 0x0002 | 0, 0);
+            timerW.Enabled = false;
+            timerAD.Enabled = false;
+            timerLMB.Enabled = false;
+            timerRBM.Enabled = false;
+            timerEnter.Enabled = false;
+            timerR.Enabled = false;
+        }
+
+        private void timerW_Tick(object sender, EventArgs e)
+        {
+            keybd_event((byte)0x57, 0, 0x0001 | 0, 0);
+        }
+
+        private void timerAD_Tick(object sender, EventArgs e)
+        {
+            List<int> list = new List<int>();
+            List<int> delays = new List<int>();
+
+            list.Add(0x41);
+            list.Add(0x44);
+
+            delays.Add(600);
+            delays.Add(800);
+            delays.Add(1000);
+
+            int r = rnd.Next(list.Count);
+            int key = list[r];
+            int rd = rnd.Next(delays.Count);
+            int time = delays[rd];
+
+            keybd_event((byte)key, 0, 0x0001 | 0, 0);
+            Thread.Sleep(time);
+            keybd_event((byte)key, 0, 0x0002 | 0, 0);
+        }
+
+        private void timerLMB_Tick(object sender, EventArgs e)
+        {
+            List<int> list = new List<int>();
+
+            list.Add(600);
+            list.Add(800);
+            list.Add(1000);
+
+            int r = rnd.Next(list.Count);
+            int time = list[r];
+
+            keybd_event((byte)0x01, 0, 0x0001 | 0, 0);
+            Thread.Sleep(time);
+            keybd_event((byte)0x01, 0, 0x0002 | 0, 0);
+        }
+
+        private void timerRBM_Tick(object sender, EventArgs e)
+        {
+            keybd_event((byte)0x02, 0, 0x0001 | 0, 0);
+            Thread.Sleep(100);
+            keybd_event((byte)0x02, 0, 0x0002 | 0, 0);
+            Thread.Sleep(100);
+            keybd_event((byte)0x02, 0, 0x0001 | 0, 0);
+            Thread.Sleep(100);
+            keybd_event((byte)0x02, 0, 0x0002 | 0, 0);
+        }
+
+        private void timerEnter_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{ENTER}");
+        }
+
+        private void timerR_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("r");
+        }
+    }
+}
